aeba95a9c5a1a0209e9bed1ac81c64f7b75b0c65
bilgates27 <bilgates270703@gmail.com>
2024-07-01 22:53:42 +0530

    first commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 const io = socketio(server, {
     cors: {
-        origin: "https://main--chat-application-socketio.netlify.app/" || "http://localhost:3000",
+        origin: "https://main--chat-application-socketio.netlify.app/",
         methods: ["GET", "POST"]
     }
 });
