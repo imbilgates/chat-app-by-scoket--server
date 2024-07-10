89a1f15b86a99e7e806c71739f9864289a90f1e0
bilgates27 <bilgates270703@gmail.com>
2024-07-10 09:47:50 +0530

    user-image committed

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ const io = socketio(server, {
 });
 
 io.on('connection', (socket) => {
-    socket.on('join', ({ name, room }, callback) => {
-        const { error, user } = addUser({ id: socket.id, name, room });
+
+    socket.on('join', ({ name, room, image }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room, image });
 
         if (error) return callback && callback(error);
 
         socket.emit('message', { user: 'bot', text: `${user.name}, welcome to the room ${user.room}` });
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`, image: `${image}`});
 
         socket.join(user.room);
 
@@ -39,7 +40,7 @@ io.on('connection', (socket) => {
         const user = getUser(socket.id);
       
         if (user) {
-          io.to(user.room).emit('message', { user: user.name, text: message });
+          io.to(user.room).emit('message', { user: user.name, text: message, image: user.image });
           io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });  // Ensure roomData is sent after message
         }
       
@@ -51,7 +52,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id);
 
         if(user) {
-            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left!` }); 
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left!`,  image: `${user.image}` }); 
             io.to(user.room).emit('roomData', { room: user.room , users: getUsersInRoom(user.room) });
         }
     });
